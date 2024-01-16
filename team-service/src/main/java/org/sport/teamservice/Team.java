4b213015b11b7db50c3ee921812e2fefff87dcab
raversa001 <raversa@freshdelmonte.com>
2024-01-16 15:21:31 +0100

    Updated team-service to match stats-service

--- team-service/src/main/java/org/sport/teamservice/Team.java
+++ team-service/src/main/java/org/sport/teamservice/Team.java
@@ -1,15 +1,39 @@
 package org.sport.teamservice;
 
+import org.sport.playerservice.Player;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import java.util.List;
+
 public class Team {
     private Long id;
     private String name;
+    private int matchesPlayed;
+    private int matchesWon;
+    private int matchesDrawn;
+    private int matchesLost;
+    private int goalsFor;
+    private int goalsAgainst;
+    private List<Player> players;
 
     public Team() {
+        // Constructeur par défaut
     }
 
-    public Team(Long id, String name) {
+    public Team(Long id, String name, int matchesPlayed, int matchesDrawn, int matchesLost, int matchesWon, int goalsFor, int goalsAgainst) {
         this.id = id;
         this.name = name;
+        this.matchesPlayed = matchesPlayed;
+        this.matchesDrawn = matchesDrawn;
+        this.matchesLost = matchesLost;
+        this.matchesWon = matchesWon;
+        this.goalsFor = goalsFor;
+        this.goalsAgainst = goalsAgainst;
+    }
+
+    public List<Player> getPlayers() {
+        return this.players;
     }
 
     public Long getId() {
@@ -27,4 +51,83 @@ public class Team {
     public void setName(String name) {
         this.name = name;
     }
+
+    public int getMatchesPlayed() {
+        return matchesPlayed;
+    }
+
+    public void setMatchesPlayed(int matchesPlayed) {
+        this.matchesPlayed = matchesPlayed;
+    }
+
+    public int getMatchesWon() {
+        return matchesWon;
+    }
+
+    public void setMatchesWon(int matchesWon) {
+        this.matchesWon = matchesWon;
+    }
+
+    public int getMatchesDrawn() {
+        return matchesDrawn;
+    }
+
+    public void setMatchesDrawn(int matchesDrawn) {
+        this.matchesDrawn = matchesDrawn;
+    }
+
+    public int getMatchesLost() {
+        return matchesLost;
+    }
+
+    public void setMatchesLost(int matchesLost) {
+        this.matchesLost = matchesLost;
+    }
+
+    public int getGoalsFor() {
+        return goalsFor;
+    }
+
+    public void setGoalsFor(int goalsFor) {
+        this.goalsFor = goalsFor;
+    }
+
+    public int getGoalsAgainst() {
+        return goalsAgainst;
+    }
+
+    public void setGoalsAgainst(int goalsAgainst) {
+        this.goalsAgainst = goalsAgainst;
+    }
+
+    // Méthodes pour incrémenter les statistiques
+    public void incrementMatchesPlayed() {
+        matchesPlayed++;
+    }
+
+    public void incrementMatchesWon() {
+        matchesWon++;
+    }
+
+    public void incrementMatchesDrawn() {
+        matchesDrawn++;
+    }
+
+    public void incrementMatchesLost() {
+        matchesLost++;
+    }
+
+    public void incrementGoalsFor() {
+        goalsFor++;
+    }
+
+    public void incrementGoalsAgainst() {
+        goalsAgainst++;
+    }
+
+    public void setPlayers(List<Player> lPlayer) {
+        this.players = lPlayer;
+    }
+
+    // Autres méthodes au besoin
 }
--- team-service/src/main/java/org/sport/teamservice/TeamController.java
+++ team-service/src/main/java/org/sport/teamservice/TeamController.java
@@ -1,26 +1,56 @@
 package org.sport.teamservice;
 
+import org.sport.playerservice.Player;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/teams")
 public class TeamController {
     public List<Team> teams = new ArrayList<>();
 
+    private static final String PLAYER_SERVICE_URL = "http://localhost:9092"; // URL du service Player
+
     public TeamController() {
         // Initialisation avec deux équipes de données lambda
-        teams.add(new Team(1L, "Équipe A"));
-        teams.add(new Team(2L, "Équipe B"));
+        teams.add(new Team(1L, "Équipe A", 3, 0, 1, 2, 4, 1));
+        teams.add(new Team(2L, "Équipe B", 3, 0, 2, 1, 1, 4));
     }
 
     @GetMapping("/{id}")
-    public Team getTeamById(@PathVariable Long id) {
-        return teams.stream()
-                .filter(team -> team.getId().equals(id))
+    public ResponseEntity<Team> getTeamDetails(@PathVariable Long id) {
+        // Recherchez l'équipe avec l'ID correspondant à {id} dans votre liste d'équipes
+        Team team = teams.stream()
+                .filter(t -> t.getId().equals(id))
                 .findFirst()
                 .orElse(null);
+
+        if (team != null) {
+            // Utilisez RestTemplate pour effectuer un appel GET au service Player pour récupérer la liste de tous les joueurs
+            String playersUrl = PLAYER_SERVICE_URL + "/players/all";
+            RestTemplate restTemplate = new RestTemplate();
+            Player[] allPlayersArray = restTemplate.getForObject(playersUrl, Player[].class);
+
+            // Convertissez le tableau de joueurs en une liste
+            List<Player> allPlayers = Arrays.asList(allPlayersArray);
+
+            // Filtrer les joueurs ayant le même teamId que l'équipe trouvée
+            List<Player> teamMembers = allPlayers.stream()
+                    .filter(player -> player.getTeamId().equals(id))
+                    .collect(Collectors.toList());
+
+            // Ajouter la liste des joueurs membres de l'équipe aux détails de l'équipe
+            team.setPlayers(teamMembers);
+        }
+
+        return ResponseEntity.ok(team);
     }
 
     @PostMapping
